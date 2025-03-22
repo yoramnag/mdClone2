8f0ec673d6612b054d1e993f48fdac74cf26c626
yoram <yoramnag@gmail.com>
2025-03-22 20:59:33 +0200

    add new project

---
+++ src/main/java/com/example/mdClone2/MdClone2Application.java
@@ -0,0 +1,13 @@
+package com.example.mdClone2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdClone2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdClone2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mdClone2/MdClone2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdClone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdClone2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
